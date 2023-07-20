86b56438dc92f8952e5643f56298d1a542e8f632
AlbertineN <albertinengingan@gmail.com>
2023-07-20 13:53:03 +0300

    Implmented some error handling techniques using go

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,14 +3,13 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -22,31 +21,29 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err)
 		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err)
 	}
 }
 
-func tinny(w http.ResponseWriter, r *http.Request) {
+func (app *application)tinny(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/tinny" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
 	w.Write([]byte("Tinny you are soo awesome..."))
 }
 
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application)showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1{
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -55,10 +52,10 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application)createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		w.Header().Set("Allow", "POST")
-		http.Error(w, "Method Not Allowe", 405)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,22 +1,37 @@
 package main
 
-import	(
+import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type application struct {
+	errorLog*log.Logger
+	infoLog *log.Logger
+}
+
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/tinny", tinny)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
 
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	log.Println("Starting server  on port :4000")
-	err :=http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	app := &application{
+		errorLog: errorLog,
+		infoLog: infoLog,
+	}
+
+	srv := &http.Server{
+		Addr: *addr,
+		ErrorLog: errorLog,
+		Handler: app.routes(),
+	}
+
+	infoLog.Printf("Starting server  on %s", *addr)
+	err :=srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
