1b77ac420c505955080ad91baa4e9c2cfdf2720f
AlbertineN <albertinengingan@gmail.com>
2023-07-22 14:07:19 +0300

    Added a db connector for my snippet application

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"database/sql" //db import
 	"flag"
 	"log"
 	"net/http"
 	"os"
+	_ "github.com/go-sql-driver/mysql" //db import
 )
 
 type application struct {
@@ -17,8 +19,20 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
+	// a db command-line flag
+
+	dsn := flag.String("dsn", "web:Pass123#@/snippetbox?parseTime=true", "MySQL data source name")
+	flag.Parse()
+
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -32,6 +46,17 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server  on %s", *addr)
-	err :=srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+	return nil, err
+	}
+	if err = db.Ping(); err != nil {
+	return nil, err
+	}
+	return db, nil
+	}
